3c4bcb9ed1ea9564a0f90d43110113bdeeac795a
tamer bilici <tamerbilici00@gmail.com>
2023-08-21 12:51:45 +0300

    exception

---
+++ src/main/java/com/tamerb/exception/TamerBiliciException.java
@@ -0,0 +1,8 @@
+package com.tamerb.exception;
+
+public class TamerBiliciException extends RuntimeException{
+
+        public TamerBiliciException(String message) {
+            super(message);
+        }
+}
